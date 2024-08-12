ab4b042a4894d744c32b4a7bdd67563f1491b9a2
wankhairul11 <w.k.aiman1993@gmail.com>
2024-08-12 12:30:09 +0800

    files added

--- homestaybooking/src/index.js
+++ homestaybooking/src/index.js
@@ -6,9 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  React.createElement(React.StrictMode, null, React.createElement(App, null))
 );
 
 // If you want to start measuring performance in your app, pass a function
