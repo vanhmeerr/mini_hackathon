5b304474dcd584848806269a17a61ed54ace44df
wankhairul11 <w.k.aiman1993@gmail.com>
2024-08-12 16:31:13 +0800

    files updated

--- homestaybooking/src/App.js
+++ homestaybooking/src/App.js
@@ -1,8 +1,32 @@
 import React from 'react';
-import SearchBar from './components/SearchBar/SearchBar';
+import Login from './Login/Login';
+import Signup from './Signup/Signup';
+import Home from './home/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ForgotPassword from './ForgotPassword/ForgotPassword';
+import Bookingpage from './Build/Bookingpage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import List from './List/List';
+
 
 function App() {
-  return React.createElement('div', { className: 'App' }, React.createElement(SearchBar, null));
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/list" element={<List />} />
+          <Route path="/search" element={<Signup />} /> 
+          {/* nanti tukar */}
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/booking" element={<Bookingpage />} />
+
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
