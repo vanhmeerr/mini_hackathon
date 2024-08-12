db9fdf7c06e2c7705c3dbb100b7781ba7a62174b
Maisarah Ibrahim <maisarahibrahim2511@gmail.com>
2024-08-12 14:38:16 +0800

    Update Bookingpage.js

--- homestaybooking/src/Build/Bookingpage.js
+++ homestaybooking/src/Build/Bookingpage.js
@@ -1,89 +1,15 @@
-import { Container, Navbar, Nav, Carousel, Col, Row, Form, Button, FormGroup, Card } from 'react-bootstrap';
+import { Container, Carousel, Col, Row, Form, Button, FormGroup, Card } from 'react-bootstrap';
+import React from 'react';
+import Navbar from '../navbar/Navbar';
+import { useNavigate } from 'react-router-dom';
+
+
 
 const Bookingpage = () => {
     return (
         <div>
-            <section id='Photo'>
-            <Container className="my-4" >
-                <Row className="justify-content-center">
-                    <Col md={6}>
-                        <Carousel>
-                            <Carousel.Item>
-                                <img
-                                    className="d-block w-100"
-                                    src="https://a0.muscache.com/im/pictures/089364fd-cf9b-45fa-a592-6c2299fc307e.jpg?im_w=1200"
-                                    alt="First slide"
-                                />
-                                <Carousel.Caption>
-                                    <h3>First Slide Label</h3>
-                                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                                </Carousel.Caption>
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <img
-                                    className="d-block w-100"
-                                    src="https://a0.muscache.com/im/pictures/1a11877f-971c-4a65-8293-4cdc6e2bc718.jpg?im_w=720"
-                                    alt="Second slide"
-                                />
-                                <Carousel.Caption>
-                                    <h3>Second Slide Label</h3>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                </Carousel.Caption>
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <img
-                                    className="d-block w-100"
-                                    src="https://a0.muscache.com/im/pictures/26401432-557b-4ac8-980d-0aff64414714.jpg?im_w=1200"
-                                    alt="Third slide"
-                                />
-                                <Carousel.Caption>
-                                    <h3>Third Slide Label</h3>
-                                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                                </Carousel.Caption>
-                            </Carousel.Item>
-                        </Carousel>
-                    </Col>
-                    <Col md={6}>
-                        <Carousel>
-                            <Carousel.Item>
-                                <img
-                                    className="d-block w-100"
-                                    src="https://images.pexels.com/photos/13326362/pexels-photo-13326362.jpeg?auto=compress&cs=tinysrgb&w=600"
-                                    alt="First slide"
-                                />
-                                <Carousel.Caption>
-                                    <h3>First Slide Label</h3>
-                                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                                </Carousel.Caption>
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <img
-                                    className="d-block w-100"
-                                    src="https://images.pexels.com/photos/13454560/pexels-photo-13454560.jpeg?auto=compress&cs=tinysrgb&w=600"
-                                    alt="Second slide"
-                                />
-                                <Carousel.Caption>
-                                    <h3>Second Slide Label</h3>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                </Carousel.Caption>
-                            </Carousel.Item>
-                            <Carousel.Item>
-                                <img
-                                    className="d-block w-100"
-                                    src="https://images.pexels.com/photos/15595781/pexels-photo-15595781/free-photo-of-a-row-of-wooden-chairs-on-the-side-of-a-building.jpeg?auto=compress&cs=tinysrgb&w=600"
-                                    alt="Third slide"
-                                />
-                                <Carousel.Caption>
-                                    <h3>Third Slide Label</h3>
-                                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                                </Carousel.Caption>
-                            </Carousel.Item>
-                        </Carousel>
-                    </Col>
-                </Row>
-            </Container>
-            </section>
-            <Navbar bg="dark" variant="dark" sticky="top">
+            
+            {/* <Navbar bg="dark" variant="dark" sticky="top">
                 <Container>
                     <Nav className='me-auto'>
                         <Nav.Link href="#Photo">Photo</Nav.Link>
@@ -91,7 +17,8 @@ const Bookingpage = () => {
                         <Nav.Link href="#Price">Pricing</Nav.Link>
                     </Nav>
                 </Container>
-            </Navbar>
+            </Navbar> */}
+            <Navbar />
             <section id='Features'></section>
             <Container className="my-4">
                 <Row className="justify-content-center">
